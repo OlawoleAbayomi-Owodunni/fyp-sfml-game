a5163b62c3d217bbf45f1fc272ae7ed2980b5608
WollyTech <ayolawole@gmail.com>
2026-02-25 01:43:19 +0000

    Enemy class setup

--- project-rival/project-rival/Enemy.cpp
+++ project-rival/project-rival/Enemy.cpp
@@ -1,13 +1,21 @@
 #include "Enemy.h"
+using namespace sf;
+
 
 void Enemy::init()
 {
+	e_body.setSize(sf::Vector2f(100.f, 100.f));
+	Vector2f size = e_body.getSize();
+	e_body.setFillColor(Color::Red);
+	e_body.setOrigin(size / 2.f);
+	e_body.setPosition(e_startPos);
 }
 
 void Enemy::update(double dt)
 {
 }
 
-void Enemy::render()
+void Enemy::render(sf::RenderWindow& window)
 {
+	window.draw(e_body);
 }
--- project-rival/project-rival/Enemy.h
+++ project-rival/project-rival/Enemy.h
@@ -4,15 +4,16 @@
 class Enemy
 {
 public:
-	Enemy() {};
+	Enemy(const sf::Vector2f pos) : e_startPos(pos) {};
 	~Enemy() {};
 	void init();
 	void update(double dt);
-	void render();
+	void render(sf::RenderWindow &window);
 
 private:
 	sf::RectangleShape e_body;
 	sf::Vector2f e_velocity{ 0.f, 0.f };
 
+	sf::Vector2f e_startPos;
 };
 
