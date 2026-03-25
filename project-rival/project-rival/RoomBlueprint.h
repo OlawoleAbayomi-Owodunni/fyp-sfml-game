b1ae46502d898f7a257aaacb7bf982d4bcdacdd5
WollyTech <ayolawole@gmail.com>
2026-03-25 05:39:48 +0000

    different room sizes

--- project-rival/project-rival/RoomBlueprint.h
+++ project-rival/project-rival/RoomBlueprint.h
@@ -50,7 +50,7 @@ struct RoomPlan {
 	// Room representation
 	int width;
 	int height;	
-	float tileSize = 78.f;
+	float tileSize = 64.f;
 
 	// Room layout
 	std::vector<Tile> tileMap;	// formula for 1D index: row * width + column
